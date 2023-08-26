8a0a3a3abedfc7628e3a24f10d1f17e65a66775a
Matthew Garrett <m.garrett@hotmail.ca>
2023-08-26 16:14:42 -0230

    Small start to Javascript

--- matchdungeon.js
+++ matchdungeon.js
@@ -1,3 +1,18 @@
 let score = 0;
 let level = 1;
 let pause = 0;
+
+function init(){
+    let up = document.getElementById("upArrow");
+    let right = document.getElementById("rightArrow");
+    let left = document.getElementById("leftArrow");
+    let down = document.getElementById("downArrow");
+
+    up.addEventListener("click", );
+    right.addEventListener("click", );
+    left.addEventListener("click", );
+    down.addEventListener("click", );
+
+}
+
+window.addEventListener("DOMContentLoaded", init());
